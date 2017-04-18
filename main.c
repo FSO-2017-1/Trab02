4c804153585c2ca89cb5f9bcf285de5b50b7e606
Karine Valença <valenca.karine@gmail.com>
2017-04-18 19:57:06 -0300

    mais coisas

--- main.c
+++ main.c
@@ -8,17 +8,18 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <time.h>
+#include <stdbool.h>
 
 #define BUFFER 256
 
-/*int alarm_stop = 0;
-unsigned int alarm_period = 30;
+int alarm_stop = 0;
+unsigned int alarm_period = 3;
 
 void on_alarm(int signal){
 	if(alarm_stop) return;
 	else alarm(alarm_period);
 	printf("HM É MESMO\n");
-}*/
+}
 
 int main(){
 	int lazy_child;
@@ -46,47 +47,51 @@ int main(){
 
 	if (lazy_child) {
 		/*Filho preguiçoso escrevendo */
-		sleep(random);
-		char msg_time[10] = __TIME__;
-		char *mensage = strcat(msg_time, ": Mensagem 1 do filho dorminhoco \n");
-	
-		/* Operação obrigatória de fechar o descritor*/
-		close(pipefd[0]);
 
-		/*Escrever no pipe*/
-		write(pipefd[1],mensage, strlen(mensage)+1);
+			char msg_time[10] = __TIME__;
+			char *mensage = strcat(msg_time, ": Mensagem 1 do filho dorminhoco \n");
+		
+			/* Operação obrigatória de fechar o descritor*/
+			close(pipefd[0]);
 
-		//printf("Informe a mensagem:\n");
-		//scanf("%s",msg_usr);
-		//close(pipefd[1]);
+			/*Escrever no pipe*/
+			write(pipefd[1],mensage, strlen(mensage)+1);
 
-		//printf("%s\n",msg_usr );
-		//sleep(5);
-		//printf("DOIDO\n");
+			//printf("Informe a mensagem:\n");
+			//scanf("%s",msg_usr);
+			//close(pipefd[1]);
+
+			//printf("%s\n",msg_usr );
+			//sleep(5);
+			//printf("DOIDO\n");
+		
 	}else{
-		// Processo Pai
-		/* Operação obrigatória de fechar o descritor*/
 		signal(SIGALRM, on_alarm);
 		alarm(alarm_period);
-		close(pipefd[1]);
-
-		/*Lê a mensagem do pipe que vem do filho preguiçoso*/
-		read(pipefd[0],msg, sizeof msg);
-		printf("A mensagem do filho preguiçoso: %s\n", msg);
-
-		//Escrita do output file
 		FILE *output = fopen("output.txt", "wb");
-		if(output == NULL){
-			printf("Erro ao abrir o arquivo!\n");
-			exit(1);
+
+		for(;;){
+			// Processo Pai
+			/* Operação obrigatória de fechar o descritor*/
+			close(pipefd[1]);
+
+			/*Lê a mensagem do pipe que vem do filho preguiçoso*/
+			read(pipefd[0],msg, sizeof msg);
+			printf("A mensagem do filho preguiçoso: %s\n", msg);
+
+			//Escrita do output file
+			
+			if(output == NULL){
+				printf("Erro ao abrir o arquivo!\n");
+				exit(1);
+			}
+
+			fprintf(output, msg);
+
+			close(pipefd[0]);
+			
 		}
-
-		fprintf(output, msg);
-
-		close(pipefd[0]);
 		fclose(output);
-
-		printf("Processo será assassinado\n");
 		kill(getpid(), SIGKILL);
 	}
 
